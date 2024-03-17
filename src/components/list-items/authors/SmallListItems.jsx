d51390b130c72ced2e7e28b9b8757f8ff091acde
Carlos Gámez <hardpx@gmail.com>
2024-03-17 10:47:13 -0600

    authors items components

---
+++ src/components/list-items/authors/SmallListItems.jsx
@@ -0,0 +1,10 @@
+const SmallAithorsListItems = ({ author }) => {
+  const { name, age } = author;
+  return (
+    <p>
+      Name: {name}, Age: {age}
+    </p>
+  );
+};
+
+export default SmallAithorsListItems;
