39c08e4e4d5b6dac35d9f043fbbd2573f6a6907d
Carlos Gámez <hardpx@gmail.com>
2024-03-19 11:21:05 -0600

    crated numbered list component

---
+++ src/components/lists/Numbered.jsx
@@ -0,0 +1,14 @@
+const NumberedList = ({ items, sourceName, ItemComponent }) => {
+  return (
+    <>
+      {items.map((item, i) => (
+        <div key={i}>
+          <h3>{i + 1}</h3>
+          <ItemComponent {...{ [sourceName]: item }} />
+        </div>
+      ))}
+    </>
+  );
+};
+
+export default NumberedList;
