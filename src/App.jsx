8f3dbd8e5c752abb8fd562a2cfb060ec1bc0e4f2
Carlos Gámez <hardpx@gmail.com>
2024-03-20 12:29:39 -0600

    added moda

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import AuthorItemsSmall from './components/list-items/authors/AuthorItemsSmall';
 import AuthorItemsLarge from './components/list-items/authors/AuthorItemsLarge';
 import BookItemsSmall from './components/list-items/books/BookItemsSmall';
 import BookItemsLarge from './components/list-items/books/BookItemsLarge';
+import { Modal } from './components/Modal';
 
 const LeftSideComp = ({ title }) => {
   return <h2>{title}</h2>;
@@ -25,22 +26,28 @@ function App() {
         // <RightSideComp title={'Right'} />
         //{' '}
       </SplitScreen> */}
-      <List
-        items={authors}
-        sourceName={'author'}
-        ItemComponent={AuthorItemsSmall}
-      />
-      <List
-        items={authors}
-        sourceName={'author'}
-        ItemComponent={AuthorItemsLarge}
-      />
-      <List items={books} sourceName={'book'} ItemComponent={BookItemsSmall} />
-      <NumberedList
-        items={books}
-        sourceName={'book'}
-        ItemComponent={BookItemsLarge}
-      />
+      <Modal>
+        <List
+          items={authors}
+          sourceName={'author'}
+          ItemComponent={AuthorItemsSmall}
+        />
+        <List
+          items={authors}
+          sourceName={'author'}
+          ItemComponent={AuthorItemsLarge}
+        />
+        <List
+          items={books}
+          sourceName={'book'}
+          ItemComponent={BookItemsSmall}
+        />
+        <NumberedList
+          items={books}
+          sourceName={'book'}
+          ItemComponent={BookItemsLarge}
+        />
+      </Modal>
     </>
   );
 }
