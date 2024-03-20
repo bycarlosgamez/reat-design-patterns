9a46753e3bfcea5dc74255a0f30418f057a7a52e
Carlos Gámez <hardpx@gmail.com>
2024-03-20 11:22:25 -0600

    added lists components

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,12 @@
-import SplitScreen from './components/SplitScreen';
+// import SplitScreen from './components/SplitScreen';
+import { authors } from './assets/data/authors';
+import { books } from './assets/data/books';
+import List from './components/lists/List';
+import NumberedList from './components/lists/Numbered';
+import AuthorItemsSmall from './components/list-items/authors/AuthorItemsSmall';
+import AuthorItemsLarge from './components/list-items/authors/AuthorItemsLarge';
+import BookItemsSmall from './components/list-items/books/BookItemsSmall';
+import BookItemsLarge from './components/list-items/books/BookItemsLarge';
 
 const LeftSideComp = ({ title }) => {
   return <h2>{title}</h2>;
@@ -10,10 +18,30 @@ const RightSideComp = ({ title }) => {
 
 function App() {
   return (
-    <SplitScreen leftWidth={1} rightWidth={3}>
-      <LeftSideComp title={'Left'} />
-      <RightSideComp title={'Right'} />
-    </SplitScreen>
+    <>
+      {/* //{' '}
+      <SplitScreen leftWidth={1} rightWidth={3}>
+        // <LeftSideComp title={'Left'} />
+        // <RightSideComp title={'Right'} />
+        //{' '}
+      </SplitScreen> */}
+      <List
+        items={authors}
+        sourceName={'author'}
+        ItemComponent={AuthorItemsSmall}
+      />
+      <List
+        items={authors}
+        sourceName={'author'}
+        ItemComponent={AuthorItemsLarge}
+      />
+      <List items={books} sourceName={'book'} ItemComponent={BookItemsSmall} />
+      <NumberedList
+        items={books}
+        sourceName={'book'}
+        ItemComponent={BookItemsLarge}
+      />
+    </>
   );
 }
 
